ba96c29e187cbcba3eff1a7992f73c10a1625c3b
Alex Proskurin <alexproskurin@gmail.com>
2019-02-23 12:17:28 -0800

    Implemented clock setting in VHDL

--- src/clock_12h.vhd
+++ src/clock_12h.vhd
@@ -9,7 +9,7 @@ entity clock_12h is
 		-- Input ports
 		clk_50, key_reset, key_hr, key_min, key_sec, key_start	:in  std_logic;
 		-- Output ports
-		sec_low, sec_high, min_low, min_high, hr_low, hr_high		:out  std_logic_vector(3 downto 0)
+		sec_l, sec_h, min_l, min_h, hr_l, hr_h		:out  std_logic_vector(3 downto 0)
 	);
 end clock_12h;
 
@@ -18,17 +18,19 @@ architecture A of clock_12h is
 
 	-- Declarations (optional)
 	signal internal_count: std_logic_vector(28 downto 0);
+	signal hr_reset: std_logic;
 	signal clk_1hz : std_logic;
+	signal sec_low, sec_high, min_low, min_high, hr_low, hr_high : integer;
 	
 begin
 	-- reduce the frequency from 50Mhz down to 1Hz
 	process(clk_50)
 	begin
 		if (clk_50'event and clk_50 ='1') then
-			if (internaL-count < 25000000) then
+			if (internal_count < 25000000) then
 				internal_count <= internal_count +1;
 			else
-				internal count <= (others => '0');
+				internal_count <= (others => '0');
 				clk_1hz <= not clk_1hz;
 			end if;
 		end if;
@@ -40,7 +42,7 @@ begin
 	min_high <= 0;
 	hr_low <= 1;
 	hr_high <= 0;
-	
+	hr_reset <= '0';
 	-- Set sec
 	process(key_sec)
 	begin
@@ -63,7 +65,7 @@ begin
 	begin
 		if (rising_edge(key_min)) then
 			if (min_low = 9) then
-				sec_low <= 0;
+				min_low <= 0;
 				if (min_high = 5) then
 					min_high <= 0;
 				else
@@ -75,19 +77,34 @@ begin
 		end if;
 	end process;
 
-		-- Set hr
+	-- Set hr
 	process(key_hr)
 	begin
 		if (rising_edge(key_hr)) then
-			if (min_low = 9) then
-				sec_low <= 0;
-				if (min_high = 5) then
-					min_high <= 0;
+			if (hr_reset = '0') then
+				if (hr_low = 9) then
+					hr_low <= 0;
+					hr_reset <= '1';
+					if (hr_high = 1) then
+						hr_high <= 0;
+					else
+						hr_high <= hr_high +1;
+					end if;
 				else
-					min_high <= min_high +1;
+					hr_low <= hr_low +1;
 				end if;
 			else
-				min_low <= min_low +1;
+				if (hr_low = 2) then
+					hr_low <= 0;
+					hr_reset <= '0';
+					if (hr_high = 1) then
+						hr_high <= 0;
+					else
+						hr_high <= hr_high +1;
+					end if;
+				else
+					hr_low <= hr_low +1;
+				end if;
 			end if;
 		end if;
 	end process;
@@ -105,4 +122,4 @@ begin
 
 	-- Generate Statement (optional)
 
-end <arch_name>;
+end A;
